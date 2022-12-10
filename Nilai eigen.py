9faf4471421ac3877d421a1f7710b380493fc006
Izzudin Adian Khusaini <99781993+izzudin17@users.noreply.github.com>
2022-12-10 19:04:49 +0700

    Update Nilai eigen.py

--- Nilai eigen.py
+++ Nilai eigen.py
@@ -1,9 +1,8 @@
 import numpy as np
 
+print("MENCARI NILAI EIGEN")
 print("NAMA KELOMPOK")
-print("1. Asih Saputri\tNPM 21081010053")
-print("2. Rizky Fatkhur Rohman \tNPM 21081010051")
-print("3. Iqbal Tegar Sahputra \tNPM 21081010061")
+print("Izzudin Adian Khusaini")
 
 #misalkan matriks
 A = np.mat("1 -3 3; 3 -5 3; 6 6 4")
